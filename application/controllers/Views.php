be503be302b7c6c1e1a046e10cb729f7865967af
JC <johncedricktee@gmail.com>
2018-03-01 14:51:50 -0800

    added ability to complete checked tasks on work page

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -19,16 +19,18 @@ class Views extends Application
         foreach ($tasks as $task)
         {
             if ($task->status != 2)
-                $undone[] = $task;
+            $undone[] = $task;
         }
         usort($undone, array("views", "orderByPriority"));
         // substitute the priority name
         foreach ($undone as $task)
-            $task->priority = $this->app->priority($task->priority);
+        $task->priority = $this->app->priority($task->priority);
         foreach ($undone as $task)
-            $converted[] = (array) $task;
+        $converted[] = (array) $task;
         // and then pass them on
         $parms = ['display_tasks' => $converted];
+        $role = $this->session->userdata('userrole');
+        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
         return $this->parser->parse('by_priority', $parms, true);
     }
 
@@ -36,11 +38,11 @@ class Views extends Application
     function orderByPriority($a, $b)
     {
         if ($a->priority > $b->priority)
-            return -1;
+        return -1;
         elseif ($a->priority < $b->priority)
-            return 1;
+        return 1;
         else
-            return 0;
+        return 0;
     }
 
     function makeCategorizedPanel($tasks)
@@ -49,4 +51,21 @@ class Views extends Application
         return $this->parser->parse('by_category', $parms, true);
     }
 
+    // complete flagged items
+    function complete() {
+    $role = $this->session->userdata('userrole');
+    if ($role != ROLE_OWNER) redirect('/work');
+
+    // loop over the post fields, looking for flagged tasks
+    foreach($this->input->post() as $key=>$value) {
+        if (substr($key,0,4) == 'task') {
+            // find the associated task
+            $taskid = substr($key,4);
+            $task = $this->tasks->get($taskid);
+            $task->status = 2; // complete
+            $this->tasks->update($task);
+        }
+    }
+    $this->index();
+    }
 }
